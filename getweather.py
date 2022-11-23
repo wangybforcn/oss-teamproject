70d814d073bdd73d9905db8d98e16ac96e3a123b
RosmontisQD <38135623+wangybforcn@users.noreply.github.com>
2022-11-23 21:03:48 +0900

    Update getweather.py
    updata get_rain func.

--- getweather.py
+++ getweather.py
@@ -55,52 +55,54 @@ def get_rain():
     rain = 'https://www.weather.go.kr/w/wnuri-fct2021/main/digital-forecast.do?'
 
     headers = {
-        'Accept': 'text/html, */*; q=0.01',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'zh-TW,zh-CN;q=0.9,zh;q=0.8,en-US;q=0.7,en;q=0.6',
-        'Connection': 'keep-alive',
-        'Cookie': 'JSESSIONID=3GHyi2nFv9LIkLXQOmQL4QEqlS4NSToPrM00aoPt.standalone; XTVID=A221008140211914803; xloc=1920X1080; _harry_lang=zh-TW; _harry_fid=hh-1118589055; _TRK_UID=93c1ddfca21712e65be484450bf18be7:7; _TRK_SID=5a7d0e60fd0818baa85557362d629bd7; _TRK_CR=https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=%EA%B0%95%EC%88%98%EB%9F%89; _harry_ref=https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=%EA%B0%95%EC%88%98%EB%9F%89; _harry_url=https://www.weather.go.kr/w/weather/forecast/short-term.do; _harry_hsid=A221120104739993915; _harry_dsid=A221120104739993831; _TRK_EX=8',
-        'Host': 'www.weather.go.kr',
-        'If-None-Match': '"0:28b3"',
-        'Referer': 'https://www.weather.go.kr/w/index.do',
-        'sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"windows"',
-        'Sec-Fetch-Dest': 'empty',
-        'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Site': 'same-origin',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
-        'X-Requested-With': 'XMLHttpRequest'
+    'Accept': 'text/html, */*; q=0.01',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'zh-TW,zh-CN;q=0.9,zh;q=0.8,en-US;q=0.7,en;q=0.6',
+    'Connection': 'keep-alive',
+    'Cookie': 'JSESSIONID=3GHyi2nFv9LIkLXQOmQL4QEqlS4NSToPrM00aoPt.standalone; XTVID=A221008140211914803; xloc=1920X1080; _harry_lang=zh-TW; _harry_fid=hh-1118589055; _TRK_UID=93c1ddfca21712e65be484450bf18be7:7; _TRK_SID=5a7d0e60fd0818baa85557362d629bd7; _TRK_CR=https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=%EA%B0%95%EC%88%98%EB%9F%89; _harry_ref=https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=%EA%B0%95%EC%88%98%EB%9F%89; _harry_url=https://www.weather.go.kr/w/weather/forecast/short-term.do; _harry_hsid=A221120104739993915; _harry_dsid=A221120104739993831; _TRK_EX=8',
+    'Host': 'www.weather.go.kr',
+    'Referer': 'https://www.weather.go.kr/w/index.do',
+    'sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"windows"',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-origin',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
+    'X-Requested-With': 'XMLHttpRequest'
     }
 
     kw = {
-        'code': '1135056000',
+        'code': '1159068000',
         'unit': 'm/s',
         'hr1': 'Y'
     }
 
     response = requests.get(rain, headers=headers, params=kw)
     soup = BeautifulSoup(response.content.decode(), 'lxml')
-    tagfirstul = soup.find(class_='item vs-item')
-    soup2 = BeautifulSoup(str(tagfirstul.prettify()), 'lxml')
 
-    f = open('./weatherinfo.txt', 'a+', encoding='utf-8')
+    soup = BeautifulSoup(response.content.decode(), 'lxml')
+    rainlink = soup.body.find_all(class_ = 'item-wrap')
+    souprainlink = BeautifulSoup(str(rainlink), 'lxml')
 
-    tagneed = soup2.li
-    for i in range(5):
-        tagneed = tagneed.find_next('li')
-    f.write(tagneed.get_text())
-
-    for times in range(4):
-
-        tagneed = soup2.li.find_next('li')
-        for i in range(4):
-            tagneed = tagneed.find_next('li')
-
-        f.write(tagneed.get_text())
+    with open('./weatherinfo.txt', 'a', encoding='utf-8') as f:
         f.write('\n')
 
-        tagfirstul = tagfirstul.find_next(class_='item vs-item')
-        soup2 = BeautifulSoup(str(tagfirstul), 'lxml')
+        tagul = souprainlink.ul
+        tagli = tagul.li
+        for i in range(5):
+            tagli = tagli.find_next('li')
+        f.write(tagli.get_text())
+        f.write('\n')
+        i = 0
 
-    f.close()
+
+        for i in range(12):
+            tagul = tagul.find_next('ul')
+            tagli = tagul.li
+            for i in range(5):
+                tagli = tagli.find_next('li')
+            f.write(tagli.get_text())
+            f.write('\n')
+            
+
